5522beafaa54b18f97bafecf7b2928b8d5cd8761
naitikdiwakar-web <naitik_121@hotmail.com>
2026-06-06 09:05:47 +0530

    Initial commit

---
+++ graph.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4]
+y=[5,6,7,8]
+
+plt.plot(x,y)  #line plot
+plt.title("Simple bar Graph")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
+
+
+
+
+
+
+
